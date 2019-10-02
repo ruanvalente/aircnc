08073f1e52ade2a221c4eb81e4135b7687097a09
ruanvalente <ruanopensuselinux@gmail.com>
2019-10-02 13:02:33 -0300

    Feita a página de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Routes from './routes'
 import './App.css'
 import logo from './assets/logo.svg'
 
@@ -8,24 +9,7 @@ function App () {
     <div className='container'>
       <img src={logo} alt='AirCnC logo' />
       <div className='content'>
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre{' '}
-          <strong>talentos</strong> para sua empresa.
-        </p>
-
-        <form>
-          <label htmlFor='email'>E-MAIL *</label>
-          <input
-            type='email'
-            name='email'
-            id='email'
-            placeholder='Seu melhor e-mail.'
-          />
-
-          <button className='btn' type='submit'>
-            Entrar
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   )
