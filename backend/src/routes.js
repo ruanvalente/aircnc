f207d18807a80999b58cb827355ded3277114d69
ruanvalente <ruanopensuselinux@gmail.com>
2019-10-02 09:23:36 -0300

    Adicionado os modelos e as rotas.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,19 @@
 const express = require('express')
-const routes = express.Router()
+const multer = require('multer')
+const uploadConfig = require('./config/upload')
 
-routes.get('/', (req, res) => {
-  return res.send('Hello word')
-})
+const SessionController = require('./controllers/SessionController')
+const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
+
+const routes = express.Router()
+const upload = multer(uploadConfig)
+
+routes.post('/sessions', SessionController.store)
+routes.get('/spot', SpotController.index)
+routes.post('/spots', upload.single('thumbnail'), SpotController.store)
+routes.get('/dashboard', DashboardController.show)
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 module.exports = routes
