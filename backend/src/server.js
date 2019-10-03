bed42ce92b5bd764eb32a5d4bce2d7d0bb4d2ddf
ruanvalente <ruanopensuselinux@gmail.com>
2019-10-02 21:30:01 -0300

    Feita a alteração para servir os arquivos estáticos thumbnail.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const path = require('path')
 const routes = require('./routes')
 
 const app = express()
@@ -15,5 +16,6 @@ mongoose.connect(
 
 app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 app.listen(3333)
