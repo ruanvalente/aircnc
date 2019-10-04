8772d2ee5682b667214ed85621274ea0c1127eee
ruanvalente <ruanopensuselinux@gmail.com>
2019-10-04 09:40:32 -0300

    Terminado a tela de List e feita a integração com a api.

--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -1,6 +1,24 @@
 import React from 'react'
-import { View } from 'react-native'
+import {
+  View,
+  Text,
+  Platform,
+  StyleSheet,
+  SafeAreaView,
+  AsyncStorage
+} from 'react-native'
 
-export default function Book () {
-  return <View />
+export default function Book ({ navigation }) {
+  const id = navigation.getParam('id')
+  return (
+    <View style={styles.container}>
+      <Text>{id}</Text>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Platform.OS === 'android' ? 40 : SafeAreaView
+  }
+})
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,49 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {
+  View,
+  SafeAreaView,
+  Platform,
+  Image,
+  StyleSheet,
+  AsyncStorage,
+  ScrollView
+} from 'react-native'
+
+import SpotList from '../components/SpotList'
+
+import logo from '../assets/logo.png'
 
 export default function List () {
-  return <View />
+  const [techs, setTechs] = useState([])
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs
+        .split(',')
+        .map(storagedTechs => storagedTechs.trim())
+      setTechs(techsArray)
+    })
+  }, [])
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    marginTop: Platform.OS === 'android' ? 50 : SafeAreaView,
+    alignSelf: 'center'
+  }
+})
